5c9a6359ee6a65be856dc049ed3e223e04de27e9
mha <jiacheng.chen2929@gmail.com>
2024-04-19 21:09:53 +0800

    version1.0.1

---
+++ Git.py
@@ -0,0 +1,18 @@
+# 工作区分为已管理，新修改的部分
+# 工作区内容提交到暂存区 用下面命令，文件会变成绿色
+# git add .
+# 暂存区检验无问题后提交到版本库，用commit命令
+# git commit -m'更改的版本名称'
+# 暂存区用来缓存
+#   git init
+#   git config --global user.email "you@example.com"
+#   git config --global user.name "Your Name"
+
+#   git status
+#   git log
+
+"""
+回滚
+
+
+"""
